24cb7c1fff0dd5136a13f159c41f15fc0d4d8706
erowan <erowan@iswleuven.be>
2017-04-13 15:56:08 +0200

    sort of.... half fixed stuff

--- binPackingVisualisation/test.js
+++ binPackingVisualisation/test.js
@@ -11,33 +11,37 @@ function submitform() {
 }
 
 
-var renderer, stage, layers, rect, counter;
+var renderer, stage, layers, rect, layerCounter, containerCounter ;
 
 function generateContainerLayers(obj) {
+    containerCounter=0;
     obj.forEach(function (entry) {
-        counter = 1;
+        layerCounter = 0;
+        containerCounter++;
         var locations = entry[0].items;
         layers = calculateZvalues(locations);
+
+        var divtag= $("<div id=\"containerdiv"+containerCounter+"\" >");
+        $("#visualisation").append(divtag);
+
         var header = $("<h1>Container " + entry[0].length + " x " + entry[0].width + " x " + entry[0].height + "</h1>");
         var intro = $("<p>This container has to be filled " + entry[1] + " times and will contain " + locations.length + " items.</p>");
-        $("#visualisation").append(header);
-        $("#visualisation").append(intro);
+        $("#containerdiv"+containerCounter).append(header);
+        $("#containerdiv"+containerCounter).append(intro);
 
-        var divtag= $("<div id=\"containerdiv\" style='float'>");
-        $("#visualisation").append(divtag);
         layers.sort();
         layers.forEach(function (zvalue) {
-            divtag = $("<div id=\"layer"+counter+"\">");
-            var layertekst = $("<i>Layer " + counter + "</i><br>");
-            $("#visualisation").append(divtag);
-            $("#visualisation").append(layertekst);
-            counter++;
+            layerCounter++;
+            divtag = $("<div id=\"layer"+containerCounter+""+layerCounter+"\"style=\'float: left\'>");
+            var layertekst = $("<i>Layer " + layerCounter + "</i><br>");
+            $("#containerdiv"+containerCounter).append(divtag);
+            $("#layer"+containerCounter+""+layerCounter).append(layertekst);
             renderer = PIXI.autoDetectRenderer(entry[0].length, entry[0].width, {
                 transparent: false, //create tranparent
                 backgroundColor: '0x86D0F2' //create backgroundColor
             });
 
-            $("#visualisation").append(renderer.view);// 2. Append canvas element to the body
+            $("#layer"+containerCounter+""+layerCounter).append(renderer.view);// 2. Append canvas element to the body
             stage = new PIXI.Container();// 3. Create a container that will hold your scene
 
             for (var i = 0; i < locations.length; i++) {
@@ -53,9 +57,9 @@ function generateContainerLayers(obj) {
                 }
             }
             render();// add stage to the canvas
-            $("#visualisation").append("</div>");
         })
-        $("#visualisation").append("</div>");
+        $("#visualisation").append("<br><br><br><br><br><br><br><br><br><br><br><br><br><br>");
+
     })
 }
 
