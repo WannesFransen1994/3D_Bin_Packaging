f0c36fb14538da5b0b4b36096a90880d845a465b
erowan <erowan@iswleuven.be>
2017-04-14 14:07:09 +0200

    cleaned up test files visualisation

--- binPackingVisualisation/printer.js
+++ binPackingVisualisation/printer.js
@@ -1,51 +1,60 @@
 function submitform() {
-    $.getJSON('http://localhost:8080/api/containersetup',$("form").serialize(),function (data) {
+    $.getJSON('http://localhost:8080/api/containersetup', $("form").serialize(), function (data) {
         $("div").empty();
         generateContainerLayers(data);
     });
 }
 
 
-var renderer, stage, layers, rect, counter;
+var renderer, stage, layers, rect, layerCounter, containerCounter;
 
 function generateContainerLayers(obj) {
+    containerCounter = 0;
     obj.forEach(function (entry) {
-        counter = 1;
+        layerCounter = 0;
+        containerCounter++;
         var locations = entry[0].items;
         layers = calculateZvalues(locations);
+
+        var divtag = $("<div id=\"containerdiv" + containerCounter + "\" class=container>");
+        $("#visualisation").append(divtag);
+
         var header = $("<h1>Container " + entry[0].length + " x " + entry[0].width + " x " + entry[0].height + "</h1>");
         var intro = $("<p>This container has to be filled " + entry[1] + " times and will contain " + locations.length + " items.</p>");
-        $("div").append(header);
-        $("div").append(intro);
-
+        $("#containerdiv" + containerCounter).append(header);
+        $("#containerdiv" + containerCounter).append(intro);
 
         layers.sort();
         layers.forEach(function (zvalue) {
-            var layertekst = $("<br><i>Layer " + counter + "</i><br>");
-            counter++;
-            $("div").append(layertekst);
-            renderer = PIXI.autoDetectRenderer(entry[0].length * 3, entry[0].width * 3, {
+            layerCounter++;
+            divtag = $("<div id=\"layer" + containerCounter + "" + layerCounter + "\"style=\'float: left\' class=layer>");
+            var layertekst = $("<i>Layer " + layerCounter + "</i><br>");
+            $("#containerdiv" + containerCounter).append(divtag);
+            $("#layer" + containerCounter + "" + layerCounter).append(layertekst);
+            renderer = PIXI.autoDetectRenderer(entry[0].length, entry[0].width, {
                 transparent: false, //create tranparent
                 backgroundColor: '0x86D0F2' //create backgroundColor
             });
 
-            $("div").append(renderer.view);// 2. Append canvas element to the body
-            stage = new PIXI.Container();// 3. Create a container that will hold your scene
+            $("#layer" + containerCounter + "" + layerCounter).append(renderer.view); // 2. Append canvas element to the body
+            stage = new PIXI.Container(); // 3. Create a container that will hold your scene
 
             for (var i = 0; i < locations.length; i++) {
                 rect = new PIXI.Graphics();
-                rect.beginFill(0x709FE9, 1);// define fill and rectangle size
+                rect.beginFill(0x709FE9, 1); // define fill and rectangle size
                 var object = locations[i];
                 if (object[0].coordinate_z == zvalue) {
-                    rect.lineStyle(1, 0xFF020);
-                    rect.drawRect(object[0].coordinate_x * 3, object[0].coordinate_y * 3,
-                        object[1].length * 3, object[1].width * 3);// x, y, width, height
+                    rect.lineStyle(1, 0x00000);
+                    rect.drawRect(object[0].coordinate_x, object[0].coordinate_y,
+                        object[1].length, object[1].width); // x, y, width, height
                     rect.endFill();
                     stage.addChild(rect);
                 }
             }
-            render();// add stage to the canvas
+            render(); // add stage to the canvas
         })
+
+
     })
 }
 
@@ -63,5 +72,3 @@ function calculateZvalues(locations) {
 function render() {
     renderer.render(stage);
 }
-
-
